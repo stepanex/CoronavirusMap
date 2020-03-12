dfb23810e67bad93029f1e716e652da10e869b6c
stepanex <stepanexe@gmail.com>
2020-03-12 13:21:57 +0100

    css

--- wikiData/regions.php
+++ wikiData/regions.php
@@ -11,6 +11,9 @@ function addFileToArray($array, $filename){
     return $array;
 }
 $arr = [];
+$arr['casesCount'] = 0;
+$arr['error'] = [];
+$delimeter = '';
 $cacheFilename = './regionsCache';
 $cacheRevIdFilename = './regionsCacheId';
 $wiki1 =  file_get_contents("https://cs.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Epidemie_koronaviru_SARS-CoV-2_v_%C4%8Cesku&rvsection=0&rvprop=ids|content");
@@ -20,30 +23,82 @@ if(file_exists($cacheFilename) && file_exists($cacheRevIdFilename) && strval($la
     echo file_get_contents($cacheFilename, true);
 }else{
     $wiki1Html =  $wiki1Json['query']['pages']['1570967']['revisions'][0]['*'];
-    if (strpos($wiki1Html, '| rozšíření = ') === false) {
+    //getting counts
+    try{
+        $delimeter = '| nakažení = ';
+        if (strpos($wiki1Html, $delimeter) === false) {
+            $arr = addFileToArray($arr, $cacheFilename);
+            $arr['errorCount'] = 1;
+            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
+            throw new Exception();
+        }
+        $infectedHtml = explode($delimeter,$wiki1Html)[1];
+
+        $delimeter = '| úmrtí = ';
+        if (strpos($infectedHtml, $delimeter) === false) {
+            $arr = addFileToArray($arr, $cacheFilename);
+            $arr['errorCount'] = 1;
+            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
+            throw new Exception();
+        }
+        $infectedBase = explode($delimeter,$infectedHtml)[0];
+        $deadHtml = explode($delimeter,$infectedHtml)[1];
+
+        $delimeter = '| zotavení = ';
+        if (strpos($deadHtml, $delimeter) === false) {
+            $arr = addFileToArray($arr, $cacheFilename);
+            $arr['errorCount'] = 1;
+            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
+            throw new Exception();
+        }
+        $deadBase = explode($delimeter,$deadHtml)[0];
+        $recoveredHtml = explode($delimeter,$deadHtml)[1];
+
+        $delimeter = '| opatření = ';
+        if (strpos($recoveredHtml, $delimeter) === false) {
+            $arr = addFileToArray($arr, $cacheFilename);
+            $arr['errorCount'] = 1;
+            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
+            throw new Exception();
+        }
+        $recoveredBase = explode($delimeter,$recoveredHtml)[0];
+
+        $infectedNumber = intval(explode('(', $infectedBase)[0]);
+        $deadNumber = intval(explode('(', $deadBase)[0]);
+        $recoveredNumber = intval(explode('(', $recoveredBase)[0]);
+        $arr['infected'] = $infectedNumber;
+        $arr['dead'] = $deadNumber;
+        $arr['recovered'] = $recoveredNumber;
+    } catch (Exception $e){
+    }
+    //\getting counts
+    $delimeter = '| rozšíření = ';
+    if (strpos($wiki1Html, $delimeter) === false) {
         $arr = addFileToArray($arr, $cacheFilename);
         $arr['errorCount'] = 1;
-        $arr['error'] = 'Doesnt contain "| rozšíření = ", using older cache if available.';
+        array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
         echo json_encode($arr);
         die();
     }
-    $wiki1HtmlExploded = explode('| rozšíření = ',$wiki1Html)[1];
-    if (strpos($wiki1HtmlExploded, '{{Citace elektronické') === false) {
+    $wiki1HtmlExploded = explode($delimeter,$wiki1Html)[1];
+    $delimeter = '{{Citace elektronické';
+    if (strpos($wiki1HtmlExploded, $delimeter) === false) {
         $arr = addFileToArray($arr, $cacheFilename);
         $arr['errorCount'] = 1;
-        $arr['error'] = 'Doesnt contain "{{Citace elektronické", using older cache if available.';
+        array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
         echo json_encode($arr);
         die();
     }
-    $regionsHtml = explode('{{Citace elektronické', $wiki1HtmlExploded)[0];
-    if (strpos($wiki1HtmlExploded, '[[') === false) {
+    $regionsHtml = explode($delimeter, $wiki1HtmlExploded)[0];
+    $delimeter = '[[';
+    if (strpos($wiki1HtmlExploded, $delimeter) === false) {
         $arr = addFileToArray($arr, $cacheFilename);
         $arr['errorCount'] = 1;
-        $arr['error'] = 'Doesnt contain "[[", using older cache if available.';
+        array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
         echo json_encode($arr);
         die();
     }
-    $regionsHtmlExploded =  explode('[[', $regionsHtml);
+    $regionsHtmlExploded =  explode($delimeter, $regionsHtml);
     $arr['errorCount'] = 0;
     foreach ($regionsHtmlExploded as $region ){
         if (strpos($region, ']]') !== false) {
