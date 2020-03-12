3455000a311dc383ac88c9b0c5190406c0ceabb4
stepanex <stepanexe@gmail.com>
2020-03-12 10:16:04 +0100

    Added loading from cache if thrown error

--- wikiData/regions.php
+++ wikiData/regions.php
@@ -1,31 +1,46 @@
 <?php
+function addFileToArray($array, $filename){
+    if(file_exists($filename)){
+        $fileJson = json_decode(file_get_contents($filename, true));
+        if($fileJson!=null){
+            foreach ($fileJson as $region=>$count){
+                $array[$region] = $count;
+            }
+        }
+    }
+    return $array;
+}
 $arr = [];
 $cacheFilename = './regionsCache';
 $cacheRevIdFilename = './regionsCacheId';
 $wiki1 =  file_get_contents("https://cs.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Epidemie_koronaviru_SARS-CoV-2_v_%C4%8Cesku&rvsection=0&rvprop=ids|content");
 $wiki1Json = json_decode($wiki1, true);
 $lastRevisionId = $wiki1Json['query']['pages']['1570967']['revisions'][0]['revid'];
+$lastRevisionId=14;
 if(file_exists($cacheFilename) && file_exists($cacheRevIdFilename) && strval($lastRevisionId) == file_get_contents($cacheRevIdFilename)){
-    echo file_get_contents($cacheFilename);
+    echo file_get_contents($cacheFilename, true);
 }else{
     $wiki1Html =  $wiki1Json['query']['pages']['1570967']['revisions'][0]['*'];
     if (strpos($wiki1Html, '| rozšíření = ') === false) {
+        $arr = addFileToArray($arr, $cacheFilename);
         $arr['errorCount'] = 1;
-        $arr['error'] = 'Doesnt contain "| rozšíření = ".';
+        $arr['error'] = 'Doesnt contain "| rozšíření = ", using older cache if available.';
         echo json_encode($arr);
         die();
     }
     $wiki1HtmlExploded = explode('| rozšíření = ',$wiki1Html)[1];
     if (strpos($wiki1HtmlExploded, '{{Citace elektronické') === false) {
+        $arr = addFileToArray($arr, $cacheFilename);
         $arr['errorCount'] = 1;
-        $arr['error'] = 'Doesnt contain "{{Citace elektronické".';
+        $arr['error'] = 'Doesnt contain "{{Citace elektronické", using older cache if available.';
         echo json_encode($arr);
         die();
     }
     $regionsHtml = explode('{{Citace elektronické', $wiki1HtmlExploded)[0];
     if (strpos($wiki1HtmlExploded, '[[') === false) {
+        $arr = addFileToArray($arr, $cacheFilename);
         $arr['errorCount'] = 1;
-        $arr['error'] = 'Doesnt contain "[[".';
+        $arr['error'] = 'Doesnt contain "[[", using older cache if available.';
         echo json_encode($arr);
         die();
     }
