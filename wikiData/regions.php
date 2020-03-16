1fdfe72cf6882a55f144c5787fd813d3b5877aab
stepanex <stepanexe@gmail.com>
2020-03-16 15:02:46 +0100

    Better parsing errors handling

--- wikiData/regions.php
+++ wikiData/regions.php
@@ -3,37 +3,67 @@ require_once('../database.php');
 
 function addFileToArray($array, $cachedRevidJson){
     $db = new database();
-    if($cachedRevidJson[0]!=-1){
-        $fileJson = json_decode($db->getCache('wikiInfo'),true)[1]['data'];
+    if($cachedRevidJson[0]){
+        $fileJson = json_decode(json_decode($db->getCache('wikiInfo'),true)[1]['data']);
         if($fileJson!=null){
             foreach ($fileJson as $region=>$count){
-                $array[$region] = $count;
+                if(array_key_exists($region, $array)){
+                    $array[$region] = $count;
+                }
             }
         }
     }
     return $array;
 }
 
-$db = new database();
-$pageid = '1570967';
 $arr = [];
 $arr['errorCount'] = 0;
 $arr['error'] = [];
+$arr['infected'] = null;
+$arr['dead'] = null;
+$arr['recovered'] = null;
+$arr['Praha'] = null;
+$arr['Královéhradecký kraj'] = null;
+$arr['Karlovarský kraj'] = null;
+$arr['Liberecký kraj'] = null;
+$arr['Moravskoslezský kraj'] = null;
+$arr['Olomoucký kraj'] = null;
+$arr['Pardubický kraj'] = null;
+$arr['Plzeňský kraj'] = null;
+$arr['Středočeský kraj'] = null;
+$arr['Jihočeský kraj'] = null;
+$arr['Jihomoravský kraj'] = null;
+$arr['Ústecký kraj'] = null;
+$arr['Kraj Vysočina'] = null;
+$arr['Zlínský kraj'] = null;
+
+$db = new database();
+$dbStatus = $db->getStatusArr();
+if(!($dbStatus[0])){
+    $arr['errorCount'] +=1;
+    array_push($arr['error'], $dbStatus[1]);
+    echo json_encode($arr);
+    die();
+}
+$pageid = '1570967';
 $delimeter = '';
 $wiki1 =  file_get_contents('https://cs.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&pageids='.$pageid.'&rvsection=0&rvprop=ids|content');
 $wiki1Json = json_decode($wiki1, true);
 $lastRevisionId = $wiki1Json['query']['pages'][$pageid]['revisions'][0]['revid'];
 $cachedRevidJson = json_decode($db->getCacheLastRevision('wikiInfo'), true);
 
-if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1]['revid'])){
+if($cachedRevidJson[0] && intval($lastRevisionId) == intval($cachedRevidJson[1]['revid'])){
     echo json_decode($db->getCache('wikiInfo'),true)[1]['data'];
 }else{
     $wiki1Html =  $wiki1Json['query']['pages'][$pageid]['revisions'][0]['*'];
+    $arr = addFileToArray($arr, $cachedRevidJson);
+
+    $arr['errorCount'] = 0;
+    $arr['error'] = [];
     //getting counts
     try{
         $delimeter = '| nakažení =';
         if (strpos($wiki1Html, $delimeter) === false) {
-            $arr = addFileToArray($arr, $cachedRevidJson);
             $arr['errorCount'] += 1;
             array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
             throw new Exception();
@@ -42,7 +72,6 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
 
         $delimeter = '| úmrtí =';
         if (strpos($infectedHtml, $delimeter) === false) {
-            $arr = addFileToArray($arr, $cachedRevidJson);
             $arr['errorCount'] += 1;
             array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
             throw new Exception();
@@ -52,7 +81,6 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
 
         $delimeter = '| zotavení =';
         if (strpos($deadHtml, $delimeter) === false) {
-            $arr = addFileToArray($arr, $cachedRevidJson);
             $arr['errorCount'] += 1;
             array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
             throw new Exception();
@@ -62,7 +90,6 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
 
         $delimeter = '| opatření =';
         if (strpos($recoveredHtml, $delimeter) === false) {
-            $arr = addFileToArray($arr, $cachedRevidJson);
             $arr['errorCount'] += 1;
             array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
             throw new Exception();
@@ -80,7 +107,6 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
     //\getting counts
     $delimeter = '| nakažení';
     if (strpos($wiki1Html, $delimeter) === false) {
-        $arr = addFileToArray($arr, $cachedRevidJson);
         $arr['errorCount'] += 1;
         array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
         echo json_encode($arr);
@@ -89,7 +115,6 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
     $wiki1HtmlExploded = explode($delimeter,$wiki1Html)[0];
     $delimeter = '</ref>';
     if (strpos($wiki1HtmlExploded, $delimeter) === false) {
-        $arr = addFileToArray($arr, $cachedRevidJson);
         $arr['errorCount'] += 1;
         array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
         echo json_encode($arr);
@@ -98,7 +123,6 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
     $wiki1HtmlExploded = explode($delimeter,$wiki1HtmlExploded)[1];
     $delimeter = '[[';
     if (strpos($wiki1HtmlExploded, $delimeter) === false) {
-        $arr = addFileToArray($arr, $cachedRevidJson);
         $arr['errorCount'] += 1;
         array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
         echo json_encode($arr);
@@ -108,8 +132,13 @@ if($cachedRevidJson[0] && intval($lastRevisionId) <= intval($cachedRevidJson[1][
     foreach ($regionsHtmlExploded as $region ){
         if (strpos($region, ']]') !== false) {
             $regionExploded = explode(']]', $region);
-            $regionCount = $res = preg_replace("/[^0-9]/", "", $regionExploded[1]);
-            $arr[$regionExploded[0]] = $regionCount;
+            $regionName = $regionExploded[0];
+            $regionName = str_replace(array("\n", "\r"), '', $regionName);
+
+            $regionCount = preg_replace("/[^0-9]/", "", $regionExploded[1]);
+            if(array_key_exists($regionName, $arr) && $regionCount!== "" && intval($regionCount)>=0){
+                $arr[$regionName] = $regionCount;
+            }
         }
     }
     echo json_encode($arr);
