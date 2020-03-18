90a5d615dc19f4d516f45eb78b15cb3a25c61926
stepanex <stepanexe@gmail.com>
2020-03-18 02:08:18 +0100

    fixed sk loading wiki data

--- states/SK/regionsData.php
+++ states/SK/regionsData.php
@@ -27,7 +27,6 @@ function addFileToArray($array, $cachedRevidJson){
     }
     return $array;
 }
-
 $pageid = '63300608';
 $arr = [];
 $arr['errorCount'] = 0;
@@ -78,6 +77,8 @@ if($cachedRevidJson[0] && intval($lastRevisionId) == intval($cachedRevidJson[1][
     echo json_decode($db->getCache($state, 'wikiInfo'),true)[1]['data'];
 }else {
     $arr = addFileToArray($arr, $cachedRevidJson);
+    $arr['errorCount'] = 0;
+    $arr['error']=[];
     $newUrl = 'https://en.wikipedia.org/w/api.php?action=parse&section=' . strval($sectionNumber) . '&prop=text&pageid=' . $pageid . '&format=json';
     $wiki = file_get_contents($newUrl);
     $wikiJson = json_decode($wiki, true);
@@ -90,16 +91,18 @@ if($cachedRevidJson[0] && intval($lastRevisionId) == intval($cachedRevidJson[1][
     $nodes = $finder->query("//table[contains(@class, '$classname')]");
     if(count($nodes) != 1){
         $arr['errorCount'] += 1;
-        array_push($arr['error'], 'Count of found tables != 1, using older cache if available.');
+        array_push($arr['error'], 'Count of found tables != 1.');
         echo json_encode($arr);
         die();
     }
+    // for some reason there are different indexes at server than on local
+    // these here are the good ones for server
 
-    $wikiTable = $nodes[0]->childNodes[3];
+    $wikiTable = $nodes[0]->childNodes[2];
     $tableLength = $wikiTable->childNodes->length;
-    $wikiTableHeader = $wikiTable->childNodes[0];
-    $wikiTableFooter = $wikiTable->childNodes[$tableLength - 1];
-    for ($i = 3; $i < 18; $i += 2) {
+    $wikiTableHeader = $wikiTable->firstChild;
+    $wikiTableFooter = $wikiTable->lastChild;
+    for ($i = 2; $i < 18; $i += 2) {
         $regionName = $wikiTableHeader->childNodes[$i]->nodeValue;
         $regionName = str_replace(array("\n", "\r"), '', $regionName);
         $regionName = mb_convert_encoding($regionName, 'iso-8859-1','utf-8');
@@ -108,15 +111,25 @@ if($cachedRevidJson[0] && intval($lastRevisionId) == intval($cachedRevidJson[1][
             $arr[$regionName] = intval($regionCount);
         }
     }
-    if(strpos($wikiTableHeader->childNodes[19]->nodeValue, 'Confirmed') !== false){
-        $arr['infected'] = intval($wikiTableFooter->childNodes[19]->nodeValue);
+    if(strpos($wikiTableHeader->childNodes[20]->nodeValue, 'Confirmed') !== false){
+        $arr['infected'] = intval($wikiTableFooter->childNodes[20]->nodeValue);
+    } else {
+        $arr['errorCount']+=1;
+        array_push($arr['error'], 'Confirmed number not at usual place.');
     }
-    if(strpos($wikiTableHeader->childNodes[21]->nodeValue, 'Deaths') !== false){
-        $arr['dead'] = intval($wikiTableFooter->childNodes[21]->nodeValue);
+    if(strpos($wikiTableHeader->childNodes[22]->nodeValue, 'Deaths') !== false){
+        $arr['dead'] = intval($wikiTableFooter->childNodes[22]->nodeValue);
+    } else {
+        $arr['errorCount']+=1;
+        array_push($arr['error'], 'Dead number not at usual place.');
     }
-    if(strpos($wikiTableHeader->childNodes[23]->nodeValue, 'Confirmed') !== false){
-        $arr['recovered'] = intval($wikiTableFooter->childNodes[23]->nodeValue);
+    if(strpos($wikiTableHeader->childNodes[24]->nodeValue, 'Recoveries') !== false){
+        $arr['recovered'] = intval($wikiTableFooter->childNodes[24]->nodeValue);
+    } else {
+        $arr['errorCount']+=1;
+        array_push($arr['error'], 'Recovered number not at usual place.');
     }
+
     echo json_encode($arr);
     $db->setCache($state, $lastRevisionId, json_encode($arr), 'wikiInfo');
     $db->setStateInfected($state, $arr['infected']);
