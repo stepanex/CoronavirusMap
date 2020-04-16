d4ac765d8231cd92a77315b46f4f31e31dc9da88
stepanex <stepanexe@gmail.com>
2020-04-16 23:11:52 +0200

    cz: removed data wiki fallback

--- states/CZ/regionsData.php
+++ states/CZ/regionsData.php
@@ -35,7 +35,6 @@ $arr['infected'] = null;
 $arr['dead'] = null;
 $arr['recovered'] = null;
 
-
 $infectedRegion = [];
 $infectedRegion['Praha'] = null;
 $infectedRegion['Královéhradecký kraj'] = null;
@@ -52,6 +51,7 @@ $infectedRegion['Ústecký kraj'] = null;
 $infectedRegion['Kraj Vysočina'] = null;
 $infectedRegion['Zlínský kraj'] = null;
 
+$arr['infectedRegion'] = $infectedRegion;
 
 $deadRegion =[];
 $deadRegion['Praha'] = null;
@@ -75,6 +75,8 @@ $deadRegionsCount = 0;
 
 $apify = file_get_contents('https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true');
 $apifyJson = json_decode($apify, true);
+
+
 if(isset($apifyJson['infectedByRegion']) && isset($apifyJson['infected']) && isset($apifyJson['recovered'])
     && isset($apifyJson['deceased'])){
     $arr['infected'] = $apifyJson['infected'];
@@ -94,6 +96,7 @@ if(isset($apifyJson['infectedByRegion']) && isset($apifyJson['infected']) && iss
 			}
 		}
     }
+    $arr['infectedRegion'] = $infectedRegion;
 
     if(isset($apifyJson['deceasedByRegion'])){
         $deadRegions = $apifyJson['deceasedByRegion'];
@@ -112,12 +115,6 @@ if(isset($apifyJson['infectedByRegion']) && isset($apifyJson['infected']) && iss
         $arr['deadRegion'] = $deadRegion;
     }
 
-    if($infectedRegionsCount == 14 && $deadRegionsCount == 14){
-        $arr['infectedRegion'] = $infectedRegion;
-        echo json_encode($arr);
-        die();
-    }
-
     if($infectedRegionsCount != 14){
         $arr['errorCount'] += 1;
         array_push($arr['error'], 'Apify didnt find all infected regions.');
@@ -127,106 +124,24 @@ if(isset($apifyJson['infectedByRegion']) && isset($apifyJson['infected']) && iss
         $arr['errorCount'] += 1;
         array_push($arr['error'], 'Apify didnt find all dead regions.');
     }
+
+} else {
+    if(!isset($apifyJson['infectedByRegion'])){
+        $arr['errorCount'] += 1;
+        array_push($arr['error'], 'Apify didnt find infectedByRegion.');
+    }
+    if(!isset($apifyJson['infected'])){
+        $arr['errorCount'] += 1;
+        array_push($arr['error'], 'Apify didnt find infected.');
+    }
+    if(!isset($apifyJson['recovered'])){
+        $arr['errorCount'] += 1;
+        array_push($arr['error'], 'Apify didnt find recovered.');
+    }
+    if(!isset($apifyJson['deceased'])){
+        $arr['errorCount'] += 1;
+        array_push($arr['error'], 'Apify didnt find deceased.');
+    }
 }
 
-$pageid = '1570967';
-$delimeter = '';
-$wiki1 =  file_get_contents('https://cs.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&pageids='.$pageid.'&rvsection=0&rvprop=ids|content');
-$wiki1Json = json_decode($wiki1, true);
-$lastRevisionId = $wiki1Json['query']['pages'][$pageid]['revisions'][0]['revid'];
-$cachedRevidJson = json_decode($db->getCacheLastRevision($state, 'wikiInfo'), true);
-
-if($cachedRevidJson[0] && intval($lastRevisionId) == intval($cachedRevidJson[1]['revid'])){
-    echo json_decode($db->getCache($state, 'wikiInfo'),true)[1]['data'];
-}else{
-    $wiki1Html =  $wiki1Json['query']['pages'][$pageid]['revisions'][0]['*'];
-	$errTmp = $arr['error'];
-	$errCount = $arr['errorCount'];
-    $arr = addFileToArray($arr, $cachedRevidJson);
-    $arr['errorCount'] = $errCount;
-    $arr['error'] = $errTmp;
-    //getting counts
-    try{
-        $delimeter = '| nakažení =';
-        if (strpos($wiki1Html, $delimeter) === false) {
-            $arr['errorCount'] += 1;
-            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
-            throw new Exception();
-        }
-        $infectedHtml = explode($delimeter,$wiki1Html)[1];
-
-        $delimeter = '| úmrtí =';
-        if (strpos($infectedHtml, $delimeter) === false) {
-            $arr['errorCount'] += 1;
-            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
-            throw new Exception();
-        }
-        $infectedBase = explode($delimeter,$infectedHtml)[0];
-        $deadHtml = explode($delimeter,$infectedHtml)[1];
-
-        $delimeter = '| zotavení =';
-        if (strpos($deadHtml, $delimeter) === false) {
-            $arr['errorCount'] += 1;
-            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
-            throw new Exception();
-        }
-        $deadBase = explode($delimeter,$deadHtml)[0];
-        $recoveredHtml = explode($delimeter,$deadHtml)[1];
-
-        $delimeter = '| opatření =';
-        if (strpos($recoveredHtml, $delimeter) === false) {
-            $arr['errorCount'] += 1;
-            array_push($arr['error'], 'Doesnt contain "'.$delimeter.'".');
-            throw new Exception();
-        }
-        $recoveredBase = explode($delimeter,$recoveredHtml)[0];
-
-        $infectedNumber = intval(explode('(', $infectedBase)[0]);
-        $deadNumber = intval(explode('(', $deadBase)[0]);
-        $recoveredNumber = intval(explode('(', $recoveredBase)[0]);
-        $arr['infected'] = $infectedNumber;
-        $arr['dead'] = $deadNumber;
-        $arr['recovered'] = $recoveredNumber;
-    } catch (Exception $e){
-    }
-    //\getting counts
-    $delimeter = '| nakažení';
-    if (strpos($wiki1Html, $delimeter) === false) {
-        $arr['errorCount'] += 1;
-        array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
-        echo json_encode($arr);
-        die();
-    }
-    $wiki1HtmlExploded = explode($delimeter,$wiki1Html)[0];
-    $delimeter = '</ref>';
-    if (strpos($wiki1HtmlExploded, $delimeter) === false) {
-        $arr['errorCount'] += 1;
-        array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
-        echo json_encode($arr);
-        die();
-    }
-    $wiki1HtmlExploded = explode($delimeter,$wiki1HtmlExploded)[1];
-    $delimeter = '[[';
-    if (strpos($wiki1HtmlExploded, $delimeter) === false) {
-        $arr['errorCount'] += 1;
-        array_push($arr['error'], 'Doesnt contain "'.$delimeter.'", using older cache if available.');
-        echo json_encode($arr);
-        die();
-    }
-    $regionsHtmlExploded =  explode($delimeter, $wiki1HtmlExploded);
-    foreach ($regionsHtmlExploded as $region ){
-        if (strpos($region, ']]') !== false) {
-            $regionExploded = explode(']]', $region);
-            $regionName = $regionExploded[0];
-            $regionName = str_replace(array("\n", "\r"), '', $regionName);
-
-            $regionCount = preg_replace("/[^0-9]/", "", $regionExploded[1]);
-            if(array_key_exists($regionName, $infectedRegion) && $regionCount!== "" && intval($regionCount)>=0){
-                $infectedRegion[$regionName] = $regionCount;
-            }
-        }
-    }
-    $arr['infectedRegion'] = $infectedRegion;
-    echo json_encode($arr);
-    $db->setCache($state, $lastRevisionId, json_encode($arr), 'wikiInfo');
-}
+echo json_encode($arr);
