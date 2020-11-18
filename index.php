4ed383288f7f58c135d55611de0606e5c1da643f
stepanex <stepanexe@gmail.com>
2020-11-18 12:29:52 +0100

    Added favicon

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
     <meta name="googlebot" content="index" />
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="/style.css?v=1.4" rel="stylesheet">
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
     <!--<script data-ad-client="ca-pub-8503799930198018" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
 </head>
 <body>
